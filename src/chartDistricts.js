d48d94f6e12b8e46586ad7e321209dedc8d58c8a
Shreshth Mohan <sremog@gmail.com>
2021-11-08 17:14:23 +0530

    choose metric select

--- src/chartDistricts.js
+++ src/chartDistricts.js
@@ -53,13 +53,41 @@ Promise.all([
   const svg = d3.select('#chart-container').append('svg')
   svg.attr('width', svgWidth).attr('height', svgHeight)
 
-  const srValues = censusData.map(d => parseFloat(d.sex_ratio))
-  const srMid = 1000
-  const [srMin, srMax] = d3.extent(srValues)
-  const maxGap = d3.min([srMid - srMin, srMax - srMid])
-  const srDomain = [1000 - maxGap, 1000 + maxGap]
+  // const srValues = censusData.map(d => parseFloat(d.sex_ratio))
+  // const srMid = 1000
+  // const [srMin, srMax] = d3.extent(srValues)
+  // const maxGap = d3.min([srMid - srMin, srMax - srMid])
+  // const srDomain = [1000 - maxGap, 1000 + maxGap]
 
-  const colorScale = d3.scaleSequential(d3.interpolateRdBu).domain(srDomain)
+  // const colorScale = d3.scaleSequential(d3.interpolateRdBu).domain(srDomain)
+
+  const metricOptionList = ['sex_ratio', 'literacy', 'Population']
+  const colorSchemes = {
+    sex_ratio: d3.schemeRdBu[5],
+    literacy: d3.schemePuOr[9].slice().reverse(),
+    Population: d3.schemeSpectral[9].slice().reverse(),
+  }
+  const colorInterpolate = {
+    sex_ratio: d3.interpolateRdBu,
+    literacy: t => d3.interpolatePuOr(1 - t),
+    Population: d3.interpolateSpectral,
+  }
+  // const metricOptionList = [{metric:'sex_ratio', colorScheme}, {metric:'literacy'}, {metric:'Population'}]
+  let metric = metricOptionList[1]
+
+  const metricSelect = d3.select('body').append('select').lower()
+
+  const colorScheme = d3.interpolateRdBu
+
+  const metricValues = censusData.map(d => parseFloat(d[metric]))
+  const metricDomain = d3.extent(metricValues)
+  // const colorScale = d3
+  //   .scaleSequential(colorInterpolate[metric])
+  //   .domain(metricDomain)
+  const colorScale = d3
+    .scaleQuantile()
+    .domain(metricValues)
+    .range(colorSchemes[metric])
 
   const districtMesh = topojson.mesh(
     districtsShapeData,
@@ -73,6 +101,7 @@ Promise.all([
   const stateMesh = topojson.mesh(
     statesShapeData,
     statesShapeData.objects.states,
+    (a, b) => a == b,
   )
 
   const path = d3
@@ -84,7 +113,7 @@ Promise.all([
 
   console.log(censusDataObj)
 
-  svg
+  const districts = svg
     .append('g')
     .selectAll('path')
     .data(districtsShapeGeo.features)
@@ -94,7 +123,7 @@ Promise.all([
       const code = d.properties.censuscode
 
       if (censusDataObj[code]) {
-        return colorScale(censusDataObj[code].sex_ratio)
+        return colorScale(censusDataObj[code][metric])
       } else {
         return 'gray'
       }
@@ -102,18 +131,54 @@ Promise.all([
     .on('mouseover', (e, d) => {
       const { DISTRICT, censuscode, ST_NM } = d.properties
 
-      const { 'sex_ratio': sexRatio, 'District name': district } =
+      const { [metric]: m, 'District name': district } =
         censusDataObj[censuscode]
       tooltipDiv.transition().duration(200).style('opacity', 1)
-      tooltipDiv.html(`${district}(${censuscode}), ${ST_NM}: ${sexRatio}`)
+      tooltipDiv.html(
+        `(${censuscode}) ${district}/${DISTRICT}, ${ST_NM}: ${metric}: ${m}`,
+      )
     })
+
+  metricSelect
+    .selectAll('option')
+    .data(metricOptionList)
+    .join('option')
+    .attr('value', d => d)
+    .text(d => d)
+
+  metricSelect.on('change', function (e, d) {
+    // console.log(e.target.value, d)
+    metric = this.value
+
+    const mv = censusData.map(d => parseFloat(d[metric])).sort()
+    const md = d3.extent(mv)
+
+    const cs = d3.scaleSequential(colorInterpolate[metric]).domain(md)
+
+    districts.attr('fill', d => {
+      const code = d.properties.censuscode
+
+      if (censusDataObj[code]) {
+        return cs(censusDataObj[code][metric])
+      } else {
+        return 'gray'
+      }
+    })
+  })
 
   svg
     .append('path')
-    // .attr('pointer-events', 'none')
+    .attr('pointer-events', 'none')
+    .attr('fill', 'none')
+    .attr('stroke', '#fefefe')
+    .attr('stroke-width', 0.5)
+    .attr('d', path(districtMesh))
+
+  svg
+    .append('path')
+    .attr('pointer-events', 'none')
     .attr('fill', 'none')
     .attr('stroke', '#333')
-    .attr('stroke-width', 0.5)
-    // .attr('d', path(stateMesh))
-    .attr('d', path(districtMesh))
+    .attr('stroke-width', 1)
+    .attr('d', path(stateMesh))
 })
